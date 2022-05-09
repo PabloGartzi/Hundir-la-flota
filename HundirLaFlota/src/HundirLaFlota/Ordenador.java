3353e90759672840e1867e60601df1935c38e43c
Gonzalo Sanchez <gsanchez046@ikasle.ehu.eus>
2022-05-09 17:37:55 +0200

    funciona todo

--- HundirLaFlota/src/HundirLaFlota/Ordenador.java
+++ HundirLaFlota/src/HundirLaFlota/Ordenador.java
@@ -16,8 +16,12 @@ public class Ordenador extends Jugador {
 	}
 
 	public void disparar() {
-		this.prepararDisparo(new Coordenada(ran.nextInt(10), ran.nextInt(10)), TipoDisparo.BOMBA);
-		
+		boolean tDis = ran.nextBoolean();
+		if(tDis)
+			this.prepararDisparo(new Coordenada(ran.nextInt(10), ran.nextInt(10)), TipoDisparo.BOMBA);
+		else
+			this.prepararDisparo(new Coordenada(ran.nextInt(10), ran.nextInt(10)), TipoDisparo.MISIL);
+
 	}
 	
 }
